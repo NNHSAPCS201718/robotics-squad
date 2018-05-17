d2c747b76f4bf13df628a7e08f3b2b225f0cc8e8
danillokachow <drkujawa@stu.naperville203.org>
2018-05-17 12:19:57 -0500

    helloworld
    finished helloworld

---
+++ Hello World/ControlMotor.java
@@ -0,0 +1,18 @@
+import lejos.nxt.*;
+
+/**
+ * Controls the motors
+ *
+ * @author danillo kachow
+ * @version 5/17/18
+ */
+public class ControlMotor
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Program 1");
+        Button.waitForAnyPress();
+        
+        
+    }
+}
